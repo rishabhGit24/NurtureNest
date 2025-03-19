dff30b90b505e1820bf6f875f7359a6264a031b8
rishabhGit24 <rishabhGit24@gmail.com>
2025-03-19 14:41:17 +0530

    Create tailwind.config.js --Gfonts

---
+++ nn/client/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        opensans: ['Open Sans', 'sans-serif'],
+        lato: ['Lato', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
